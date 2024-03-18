d34c73f9f00d3c9e3a3749088c43fe193235a424
kempsly <kempslysilencieux3@gmail.com>
2024-03-18 17:28:45 +0300

    Update package

--- ml-package/model-package/setup.py
+++ ml-package/model-package/setup.py
@@ -1,9 +1,7 @@
 import io
 import os
 from pathlib import Path
-
 from setuptools import find_packages, setup
-
 
 # Metadata of package
 NAME = 'loan_pred_model'
@@ -26,10 +24,11 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-
 # Load the package's __version__.py module as a dictionary.
 ROOT_DIR = Path(__file__).resolve().parent
-PACKAGE_DIR = ROOT_DIR / NAME
+PACKAGE_DIR = ROOT_DIR / 'loan_pred_model'
+
+# PACKAGE_DIR = ROOT_DIR / 'ml-package' / 'model-package' / 'loan_pred_model'
 about = {}
 with open(PACKAGE_DIR / 'VERSION') as f:
     _version = f.read().strip()
@@ -45,8 +44,8 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    package_data={'prediction_model': ['VERSION']},
+    packages=[NAME],
+    package_data={NAME: ['VERSION']},
     install_requires=list_reqs(),
     extras_require={},
     include_package_data=True,
