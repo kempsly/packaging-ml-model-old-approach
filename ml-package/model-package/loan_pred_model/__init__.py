7889e614db344045553eefb21d56d00e67032037
kempsly <kempslysilencieux3@gmail.com>
2024-03-18 23:29:31 +0300

    Version 5 added

--- ml-package/model-package/loan_pred_model/__init__.py
+++ ml-package/model-package/loan_pred_model/__init__.py
@@ -1,11 +1,6 @@
 import os
-# from loan_pred_model.config import PACKAGE_ROOT, config
-from .config import config
-
-# from loan_pred_model.config import config 
-# with open(PACKAGE_ROOT / "VERSION") as version_file:
-#     __version__ = version_file.read().strip()
+from loan_pred_model.config import config 
     
     
-with open(os.path.join(config.PACKAGE_ROOT, "VERSION")) as v_file:
+with open(os.path.join(config.PACKAGE_ROOT, 'VERSION')) as v_file:
     __version__ = v_file.read().strip()
